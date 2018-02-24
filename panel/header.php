1bf03dc8ac3c22eea687927f2712eb829fe1b958
hartuna <bartlomiejhartuna@gmail.com>
2018-02-24 20:51:29 +0100

    RWD to relations panel

--- panel/header.php
+++ panel/header.php
@@ -1,16 +1,18 @@
 <?php
-	session_start();
-	if(!isset($_SESSION['online'])){
-		header('Location: ../');
-		exit();
-	}
-	$id = $_SESSION['id'];
+session_start();
+if(!isset($_SESSION['online'])){
+	header('Location: ../');
+	exit();
+}
+$id = $_SESSION['id'];
 ?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>Social-network</title>
 	<meta charset="utf-8" />
+	<link href="default.css" rel="stylesheet" type="text/css" media="all" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	<?php
 	if($_SERVER['REQUEST_URI'] == '/social-network/panel/relations.php'){
 	?>
@@ -18,13 +20,12 @@
 	<?php	
 	}
 	?>
-	<link href="default.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body>
 	<header>
-		<nav>
-			<p>Witaj <?php echo $_SESSION['user'] . ' ' . $_SESSION['userSurname']; ?></p>
-		</nav>
 		<a href="logout.php"><button id="logout"></button></a>	
 		<a href="relations.php"><button></button></a>	
 	</header>
+	<div id="welcome">
+		<p><span>Witaj</span> <?php echo $_SESSION['user']; ?></p>
+	</div>
